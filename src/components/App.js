13ef3bee70fe57f1063fa893585d03dd6219a971
Alexoid1 <alex_zam@hotmail.es>
2020-02-20 17:24:07 -0500

    make it better

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
+function App (){
+    return(
+        <div>
+            <Header/>
+            <MemeGenerator/>
+
+        </div>
+    )
+}
+export default App
