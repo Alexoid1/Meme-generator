82c8de06300109eb00ef00731b304111f5e2c289
Alexoid1 <alex_zam@hotmail.es>
2020-11-14 12:20:12 -0500

    Updates

---
+++ precache-manifest.548ab5eb2d4d2d08658d0dbc6e131fe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895b6484f93967eccc81465a91458325",
+    "url": "/Meme-generator/index.html"
+  },
+  {
+    "revision": "e60d95a5e10aa0ef22e5",
+    "url": "/Meme-generator/static/js/2.9b39589c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Meme-generator/static/js/2.9b39589c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb3315e21497ee5c508",
+    "url": "/Meme-generator/static/js/main.1eedd4fc.chunk.js"
+  },
+  {
+    "revision": "f20df34438b9931ab62a",
+    "url": "/Meme-generator/static/js/runtime-main.395d4a0c.js"
+  }
+]);
