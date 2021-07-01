ff2295bc6bc41509cc436425ba44539086d620e9
Alexoid1 <alex_zam@hotmail.es>
2021-07-01 18:04:46 -0500

    erase extra style files

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Header.css';
-function Header() {
-    return (React.createElement("header", null,
-        React.createElement("img", { src: "http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png", alt: "Problem?", className: 'img1' }),
-        React.createElement("p", null, "Meme Generator")));
-}
-export default Header;
-//# sourceMappingURL=Header.js.map
